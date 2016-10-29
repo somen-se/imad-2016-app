997de1b38b01d4ee3f46f31fce907dbe62207008
somen-se <somen_se@yahoo.co.in>
2016-10-29 20:58:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+app.get('/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 app.get('/ABOUT.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'ABOUT.html'));
