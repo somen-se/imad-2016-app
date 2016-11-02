25b485854349a3c8c323195373238854cb324263
somen-se <somen_se@yahoo.co.in>
2016-11-02 10:19:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/q.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'q.jpg'));
 });
 
 
