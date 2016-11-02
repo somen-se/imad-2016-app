2baf9f6cb2b9567caf6df63116b4e29586d58d1d
somen-se <somen_se@yahoo.co.in>
2016-11-02 10:27:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ app.get('/ui/cropped-congruent_pentagon.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'cropped-congruent_pentagon.png'));
 });
 
+app.get('/ui/s.jpg')', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 's.jpg'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
